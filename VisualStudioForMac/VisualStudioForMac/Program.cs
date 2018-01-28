3275593885fa95cee0467d9f89334294b587c304
Flayshow <matthew.techmeier@gmail.com>
2018-01-27 20:57:28 -0500

    test
    test

---
+++ VisualStudioForMac/VisualStudioForMac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudioForMac
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
